b90cc656405c92a58822e5703f8fc06e6e42d2c6
PRAVEEN ARAVAMUDHAM <praveen.aravamudham@gmail.com>
2014-06-12 09:00:58 -0400

    first commit

---
+++ FSquareList/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FSquareList
+//
+//  Created by PRAVEEN ARAVAMUDHAN on 6/12/14.
+//  Copyright (c) 2014 PRAVEEN ARAVAMUDHAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
---
+++ FSquareList/MasterViewController.m
@@ -0,0 +1,127 @@
+//
+//  MasterViewController.m
+//  FSquareList
+//
+//  Created by PRAVEEN ARAVAMUDHAN on 6/12/14.
+//  Copyright (c) 2014 PRAVEEN ARAVAMUDHAN. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "Venue.h"
+#import <RestKit/RestKit.h>
+
+
+#define kCLIENTID @"O4N5CCDG0PD521SVU5OLX3BRIIFKHW2MOZ25XQUFHEELNPBD"
+#define kCLIENTSECRET @"QP0ITQH0NVLJHUN4JICLO10OPQS2EFNFZOPOFDTO22M5LE1S"
+
+@interface MasterViewController ()
+
+@property (nonatomic, strong) NSArray *venues;
+
+@end
+
+@implementation MasterViewController
+            
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+
+    [self configureRestKit];
+    [self loadVenues];
+}
+
+- (void)configureRestKit
+{
+    // initialize AFNetworking HTTPClient
+    NSURL *baseURL = [NSURL URLWithString:@"https://api.foursquare.com"];
+    AFHTTPClient *client = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
+    
+    // initialize RestKit
+    RKObjectManager *objectManager = [[RKObjectManager alloc] initWithHTTPClient:client];
+    
+    // setup object mappings
+    RKObjectMapping *venueMapping = [RKObjectMapping mappingForClass:[Venue class]];
+    [venueMapping addAttributeMappingsFromArray:@[@"name"]];
+    
+    // register mappings with the provider using a response descriptor
+    RKResponseDescriptor *responseDescriptor =
+    [RKResponseDescriptor responseDescriptorWithMapping:venueMapping
+                                                 method:RKRequestMethodGET
+                                            pathPattern:@"/v2/venues/search"
+                                                keyPath:@"response.venues"
+                                            statusCodes:[NSIndexSet indexSetWithIndex:200]];
+    
+    [objectManager addResponseDescriptor:responseDescriptor];
+}
+
+- (void)loadVenues
+{
+    NSString *latLon = @"37.33,-122.03"; // approximate latLon of The Mothership (a.k.a Apple headquarters)
+    NSString *clientID = kCLIENTID;
+    NSString *clientSecret = kCLIENTSECRET;
+    
+    NSDictionary *queryParams = @{@"ll" : latLon,
+                                  @"client_id" : clientID,
+                                  @"client_secret" : clientSecret,
+                                  @"categoryId" : @"4bf58dd8d48988d1e0931735",
+                                  @"v" : @"20140118"};
+    
+    [[RKObjectManager sharedManager] getObjectsAtPath:@"/v2/venues/search"
+                                           parameters:queryParams
+                                              success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+                                                  _venues = mappingResult.array;
+                                                  [self.tableView reloadData];
+                                              }
+                                              failure:^(RKObjectRequestOperation *operation, NSError *error) {
+                                                  NSLog(@"What do you mean by 'there is no coffee?': %@", error);
+                                              }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Segues
+/*
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        NSDate *object = self.objects[indexPath.row];
+        [[segue destinationViewController] setDetailItem:object];
+    }
+}
+ */
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.venues.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+    Venue *venue = self.venues[indexPath.row];
+    cell.textLabel.text = [venue name];
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+
+
+@end
